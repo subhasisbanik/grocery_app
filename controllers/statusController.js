ecde86dd7b08f7e62a51740fc0a4e4dc2e228922
Subhasis Banik <subhasisbanik@gmail.com>
2020-12-03 18:21:35 +0400

    updating service names for zuul gateway to recognize

--- controllers/statusController.js
+++ controllers/statusController.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/status', async function(req,res){
+    console.log("status api hit");
     res.write("status up");
     res.end();
 });
